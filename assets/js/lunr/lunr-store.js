c32c125c5040c4a0a09a21957f282071377e7056
dsgrafiniert <dsgrafiniert@users.noreply.github.com>
2021-10-04 15:28:53 +0000

    jekyll build from Action 0d6e4ac6fa48344934e8d686bcb6336589e828cc

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,19 @@
+var store = [{
+        "title": "Sektorenkopplung - was ist das eigentlich?",
+        "excerpt":"Smart Home bestand für mich lange Zeit aus der Reduzierung von Stromverbrauch und der Optimierung eines Heizsystems. Bis zur Installation unserer Photovoltaikanlage betrachtete ich diese beiden Sektoren (Elektrizität und Wärmeversorgung) weitestgehend unabhängig voneinander. Als Selbstversorger änderte sich meine Einstellung dazu. Warum betrachte ich diese beiden Themen eigentlich nicht ganzheitlich? Genau...","categories": ["erklaerung","glossar","sektorenkopplung"],
+        "tags": [],
+        "url": "/pages/smart-pv/erklaerung/glossar/sektorenkopplung/sektorenkopplung/",
+        "teaser": null
+      },{
+        "title": "Eingesetzte Software",
+        "excerpt":"Kommerzielle SmartHome Systeme bringen meist einen so genannten Vendor Lock-In mit sich. Das bedeutet, kauft man Komponenten eines Herstellers ist es schwierig, das Gesamtsystem mit Komponenten anderer Hersteller zu erweitern. Eine zentrale Architekturentscheidung meines SmartHome Systems war deshalb, auf Systeme zu setzen, die eine solche Kombination verschiedener Hersteller und Systeme...","categories": ["erklaerung","software"],
+        "tags": ["openhab","nodered","grafana","influxdb","intro"],
+        "url": "/pages/smart-pv/erklaerung/software/welche_software/",
+        "teaser": null
+      },{
+        "title": "Visualisierung",
+        "excerpt":"Ich bin ein Control Freak. Zumindest, was mein SmartHome angeht. Eine vernünftige Möglichkeit, gesammelte Daten zu visualisieren ist deshalb zentraler Bestandteil meiner Infrastruktur. Wichtig für mich war dabei: Ich will alle Daten an einem Fleck.   Die Ausgangssituation  sdf  ","categories": ["erklaerung","software","grafana"],
+        "tags": ["grafana","intro"],
+        "url": "/pages/smart-pv/erklaerung/software/grafana/visualisierung/",
+        "teaser": null
+      }]
